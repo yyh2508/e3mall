55bafd7b7d6eb0fc8b754c59c46f593348c6421c
yanyihang <yyh2508@163.com>
2017-09-26 13:10:49 +0800

    提交e3-manager

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,11 @@
 package cn.e3mall.service;
 
+import cn.common.pojo.DataguridPage;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	TbItem findByItemId(Long itemId);
+	
+	DataguridPage findByItemList(int page,int rows);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.common.pojo.DataguridPage;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 /**
  * 商品管理Service
@@ -20,6 +27,26 @@ public class ItemServiceImpl implements ItemService {
 	public TbItem findByItemId(Long itemId) {
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+	
+	/**
+	 * 利用PageHelper插件实现分页
+	 */
+	public DataguridPage findByItemList(int page, int rows) {
+		//设置分页
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//从结果中获取分页信息
+		PageInfo<TbItem> pageinfo = new PageInfo<>(list);
+		//获取总记录数封装到DataguridPage
+		DataguridPage dataguridPage = new DataguridPage();
+		//总记录数
+		dataguridPage.setPage(pageinfo.getTotal());
+		//当前页数据
+		dataguridPage.setRows(list);
+		return dataguridPage;
 	}
 
 }
